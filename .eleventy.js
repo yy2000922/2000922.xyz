4aaa6c7cdfb5b66c3fb77e8fbea46f700a6fcc0d
Fxanadu <deepwhite86@outlook.com>
2026-01-27 11:06:35 +0800

    fix bug

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
-const chinaMirror = require("eleventy-plugin-china-mirror");
+
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const { registerDateFilters } = require("./eleventy/config/filters");
 const { registerCollections } = require("./eleventy/config/collections");
 const { passthroughPaths } = require("./eleventy/config/passthrough");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(chinaMirror);
+
   eleventyConfig.addPlugin(syntaxHighlight);
 
   passthroughPaths.forEach((path) => eleventyConfig.addPassthroughCopy(path));
